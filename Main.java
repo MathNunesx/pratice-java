4e7e97efb7d5f95f7c2c3f427775e11d7b182610
Matheus Nunes <matheusjobs04@gmail.com>
2024-08-24 16:40:48 -0300

    Main

--- Main.java
+++ Main.java
@@ -1,2 +1,17 @@
+import javax.swing.JOptionPane;
 public class Main {
+    public static void main(String[] args) {
+
+        float valor;
+        ContaCorrente c1 = new ContaCorrente();
+        c1.cadastrarDados();
+
+        System.out.println("" + c1.imprimeDados());
+        JOptionPane.showMessageDialog(null, c1.imprimeDados());
+        valor = Float.parseFloat(JOptionPane.showInputDialog(null, "Digite o valor a sacar: "));
+        c1.sacar(valor);
+
+        System.out.println("" + c1.imprimeDados());
+
+    }
 }
